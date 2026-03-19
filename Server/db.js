ce85e9bd58b539f12f848f3808bae4f454f3d234
ygunk <ygunkun1015@gmail.com>
2026-03-19 14:33:55 +0900

    db 부분 수정

--- Server/db.js
+++ Server/db.js
@@ -8,13 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const ServerClient = new MongoClient(process.env.DB_URI);
-const dbName = "Sebastian";
+export const ServerClient = new MongoClient(process.env.DB_URI);
+export const dbName = "Sebastian";
 export  async function RunDB(){
     try{
         await ServerClient.connect();
         console.log("MongoDB 연결 완료");
 
+        //api 쓸 때 const db = req.app.locals.db로 변수 생성하고 사용 가능
         app.locals.db = ServerClient.db(dbName);
 
         app.listen(5000, () => {
@@ -26,3 +27,4 @@ export  async function RunDB(){
         process.exit(1);
     }
 }
+RunDB();
