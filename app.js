9061cd48a968980a9ddaa56a65f77095ed4d4b7e
윤성연 <s2427@e-mirim.hs.kr>
2026-04-07 15:24:20 +0900

    [Feat] :: websocket 서버 구현]

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 const express = require("express");
 const FishRouter = require("./routes/router");
 const dbConnect = require("./config/mongoDB");
+
+const { createServer } = require("http");
+const { WebSocketServer } = require("ws");
+
 const app = express();
 const PORT = 8000;
-dbConnect();
-app.use(express.json());
 
+// dbConnect();
+app.use(express.json());
 
 // API 라우터 연결
 app.use("/", FishRouter);
 
-app.listen(PORT, ()=>{
-    console.log(`http://localhost:${PORT}`);
+// HTTP 서버 생성
+const server = createServer(app);
+
+// WebSocket 서버 붙이기
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  console.log("✅WebSocket 연결됨");
+
+  ws.on("message", (message) => {
+    const data = JSON.parse(message.toString());
+    console.log("받은 데이터:", data);
+
+    // 모든 클라이언트에게 전달
+    wss.clients.forEach((client) => {
+      if (client.readyState === ws.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  });
 });
-//npm install dotenv mongoose express
+
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
+
+//npm install dotenv mongoose express ws
