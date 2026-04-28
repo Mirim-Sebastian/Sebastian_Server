079b9683b26824b2e5a920f069953e2dd2fe6422
hjzhhhj <jhj090120@gmail.com>
2026-04-29 08:16:29 +0900

    [Fix] :: 에러 응답 개선 및 코드 정리
    Co-authored-by: GitJiYeon <s2436@e-mirim.hs.kr>

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
-require("dotenv").config(); //env값 가져오기
+const mongoose = require("mongoose");
+require("dotenv").config(); //env 값 가져오기
 
-const dbConnect = async () =>{
-    try{
-        const con = await mongoose.connect(process.env.MONGODB_URL)
-        console.log("DB 성공");
-    }catch(error){
-        console.log(error);
-    }
-}
-module.exports = dbConnect
+const dbConnect = async () => {
+  try {
+    const con = await mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB 연결 성공");
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = dbConnect;
